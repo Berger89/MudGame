e2983052260bc754303894455b0917ca5600d66f
Matěj Hron <hron@spsejecna.cz>
2026-04-07 18:01:42 +0200

    commit

---
+++ MVPHerniSvet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Text;
+
+namespace MVPHerniSvet
+{
+    using System;
+    using System.Threading.Tasks;
+
+    namespace MVPHerniSvet
+    {
+        internal class Program
+        {
+            static async Task Main(string[] args)
+            {
+              
+                MudGame game = new MudGame(8888);
+
+               
+                await game.StartAsync();
+            }
+        }
+    }
+}
